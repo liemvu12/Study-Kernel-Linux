658509a0398ec14b5da47397976b3b7376805ee7
liemvu12 <liemvu009@gmai.com>
2025-05-11 17:59:59 +0700

    update Unit2_file

---
+++ unit2_file/unit2_file.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main()
+{
+    printf("Hello\n");
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
