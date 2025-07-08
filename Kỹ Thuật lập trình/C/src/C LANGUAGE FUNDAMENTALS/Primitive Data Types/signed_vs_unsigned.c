cc487a71f00d21538b84a8fdb68cb46f5dbe1545
LAPTOP-6NCLQ47F\PC <liemvu009@gmail.com>
2025-07-09 01:10:09 +0700

    Create Ky Thuat Lap Trinh

---
+++ Kỹ Thuật lập trình/C/src/C LANGUAGE FUNDAMENTALS/Primitive Data Types/signed_vs_unsigned.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    unsigned char a = 1; 
+    if (0 > a ) printf("Negative");
+
+    unsigned int u = 1;
+    int b = -4;
+    printf("%d", b + u);
+    return 0; 
+}
